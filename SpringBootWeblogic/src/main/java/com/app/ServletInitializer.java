4c5e22480fbaaa4e26198f4ca75b6bfca9b6a878
David Wu <wudc@yahoo.com>
2019-10-19 11:33:05 -0400

    Initial check-in

---
+++ SpringBootWeblogic/src/main/java/com/app/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWeblogicApplication.class);
+	}
+
+}
